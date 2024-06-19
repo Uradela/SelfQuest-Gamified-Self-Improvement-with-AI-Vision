bee5ed9d19d07bab2e17d15901f67514aca5883c
Uradela <naftali.sumarto.pongilatan-2021@fst.unair.ac.id>
2024-06-19 20:34:07 +0700

    Penambahan Profile Image di Cloud Storage di GCC 4

--- firebase.js
+++ firebase.js
@@ -1,5 +1,13 @@
-const { Firestore } = require('@google-cloud/firestore');
+const admin = require("firebase-admin");
 
-const db = new Firestore();
+const serviceAccount = require("D:/UNAIR/Semester 6/Stupen/Mengcoding/test 2/SelfQuest-Gamified-Self-Improvement-with-AI-Vision/capstone-selfquest-gamified-firebase-adminsdk-lu7ek-1c352da1a1.json");
 
-module.exports = db;
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    storageBucket: 'imagegamified' // Ganti dengan nama bucket penyimpanan Anda
+});
+
+const db = admin.firestore(); 
+const bucket = admin.storage().bucket(); // Inisialisasi bucket Firebase Storage
+
+module.exports = { db, bucket }; // Export db dan bucket
